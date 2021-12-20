db46c1b4412ae653a6e7287cb4df9992b03f9e9f
Åsa Berglund <you@example.com>
2021-12-20 10:55:36 +0100

    responsive nav bar created

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import axios from 'axios';
 import { useState } from 'react';
+import Header from './components/header';
 
 
 function App() {
-  //Functon to get data from routes
-  const [data, setData] = useState([])
-  axios.get('http://localhost:3001/routes/')
-    .then(res => {
-      console.log(res.data)
-      setData(res.data)
-    }).catch(err => {
-      console.log(err)
-    })
+  // //Functon to get data from routes
+  // const [data, setData] = useState([])
+  // axios.get('http://localhost:3001/routes/')
+  //   .then(res => {
+  //     console.log(res.data)
+  //     setData(res.data)
+  //   }).catch(err => {
+  //     console.log(err)
+  //   })
 
-    //Making an array that contains all data that has been fetched. 
-  const arr = data.map((data, index) => {
-    return (
-      <ul>
-        <li>{data.name}</li>
-      </ul>
-    )
-  })
+  //   //Making an array that contains all data that has been fetched. 
+  // const arr = data.map((data, index) => {
+  //   return (
+  //     <ul>
+  //       <li>{data.name}</li>
+  //     </ul>
+  //   )
+  // })
 
   return (
-    <div className="App">
-      {arr}
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
---
+++ src/components/header.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {useState} from 'react';
+import classes from './header.module.scss';
+
+import { BiMenu } from 'react-icons/bi';
+import { AiOutlineClose } from 'react-icons/ai';
+
+const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(true);
+    const menuTogglehandler = () =>{
+        setMenuOpen((p) =>!p);
+
+    }
+    return (
+        <header className={classes.header}>
+            <div className={classes.header__content}>
+                <h2 className={classes.header__content__logo}>mySession</h2>
+
+                <nav className={`${classes.header__content__nav} ${menuOpen? classes.isMenu:""}`}>
+                    <ul>
+                        <li>
+                            <a href="/">Page one</a>
+                        </li>
+                        <li>
+                            <a href="/">Page two</a>
+                        </li>
+                        <li>
+                            <a href="/">Logout</a>
+                        </li>
+                    </ul>
+                </nav>
+                <div className={classes.header__content__toggle}>
+                    {menuOpen ? (
+                    <BiMenu onClick={menuTogglehandler} />
+                    ):( 
+                     <AiOutlineClose onClick ={menuTogglehandler}/>
+                     )}
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Header
