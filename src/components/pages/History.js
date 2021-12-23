b9d44a6060642c76c9883a1ef2792355be0c8fb5
Åsa Berglund <you@example.com>
2021-12-23 13:02:30 +0100

    showing climbing routes with fetch

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -1,33 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import '../../App';
 import Axios from 'axios';
-import { useState } from 'react';
+import classes from '../Layout.module.scss';
+import moment from 'moment';
+
+
 
 function History() {
-  //Functon to get data from routes
-  const [data, setData] = useState([])
-  Axios.get('http://localhost:3001/routes/')
-    .then(res => {
-      console.log(res.data)
-      setData(res.data)
-    }).catch(err => {
-      console.log(err)
-    })
+    const [climbingRoutes, setClimbingRoutes] =useState([]);
 
-    //Making an array that contains all data that has been fetched. 
-  const arr = data.map((data, index) => {
+    //Use effect is going to run first time the page loads
+    useEffect (async ()=>{
+        getClimbingRoutes();
+    }, []);
+
+    const getClimbingRoutes = async()=>{
+        const response = await fetch(`http://localhost:3001/routes/`);
+        const data = await response.json();
+        setClimbingRoutes(data)
+    }
+
+    // //Functon to get data from routes
+    // const getClimbingRoutes = async()=>{
+    // //const [data, setData] = useState([])
+    // Axios.get('http://localhost:3001/routes/')
+    //     .then(res => {
+    //         console.log(res.data)
+    //         //setData(res.data)
+    //     }).catch(err => {
+    //         console.log(err)
+    //     })
+    // }
+
+    // //Making an array that contains all data that has been fetched. 
+    const arr = climbingRoutes.map((climbingRoutes => {
+        
+        return (
+            <div key="{climbingRoutes._id}" className={classes.history__log}>
+                <ul>
+                    {/* using moment package to format date */}
+                    <li><b>{climbingRoutes.date}</b></li>
+                    <li><b>Grade:</b> {climbingRoutes.grade}</li>
+                </ul>
+                <ul>
+                    <li><b>Name of route:</b> {climbingRoutes.name}</li>
+                    <li><b>Type of route:</b> {climbingRoutes.grade}</li>
+                </ul>
+                <ul>
+                    <li> <b>Location: </b></li>
+                    <li>{climbingRoutes.location}</li>
+                </ul>
+            </div>
+
+
+        )
+    }));
+
     return (
-      <ul>
-        <li>{data.name}</li>
-      </ul>
-    )
-  })
+        <div className="App">
+            <div className="App">
+                <form className="search-form">
+                    <input className="search-bar" type="text" />
+                    <button className="search-buttton" type="submit">Search</button>
+                </form>
+                <div className={classes.history}>
+                    <h1>This week:</h1>
 
-  return (
-    <div className="App">
-      {arr}
-    </div>
-  );
+                    {arr}
+                </div>
+            </div>
+        </div>
+    );
 }
 
 
