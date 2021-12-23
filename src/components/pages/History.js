80479ff017e44c6d45e2e02cd586a94d83882325
Åsa Berglund <you@example.com>
2021-12-23 13:23:06 +0100

    date format is changed

--- src/components/pages/History.js
+++ src/components/pages/History.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import '../../App';
-import Axios from 'axios';
+//import Axios from 'axios';
 import classes from '../Layout.module.scss';
 import moment from 'moment';
 
@@ -8,17 +8,26 @@ import moment from 'moment';
 
 function History() {
     const [climbingRoutes, setClimbingRoutes] =useState([]);
+    //Making a search function
+    // const[search,setSearch] = useState('');
+    //If submit button is clicked then a new state is set
+    // const[query, setQuery] = useState['']
 
     //Use effect is going to run first time the page loads
     useEffect (async ()=>{
         getClimbingRoutes();
     }, []);
-
+    //Function to get climbing routes fetched from rest-api
     const getClimbingRoutes = async()=>{
         const response = await fetch(`http://localhost:3001/routes/`);
         const data = await response.json();
         setClimbingRoutes(data)
     }
+    // //Everytime input field is changed this event is going to run. 
+    // const updateSearch = e =>{
+    //     setSearch(e.target.value);
+    //     console.log(search);
+    // }
 
     // //Functon to get data from routes
     // const getClimbingRoutes = async()=>{
@@ -39,7 +48,7 @@ function History() {
             <div key={climbingRoutes._id} className={classes.history__log}>
                 <ul>
                     {/* using moment package to format date */}
-                    <li><b>{climbingRoutes.date}</b></li>
+                    <li><b>{moment(climbingRoutes.date).format('LL')}</b></li>
                     <li><b>Grade:</b> {climbingRoutes.grade}</li>
                 </ul>
                 <ul>
@@ -59,10 +68,10 @@ function History() {
     return (
         <div className="App">
             <div className="App">
-                <form className="search-form">
-                    <input className="search-bar" type="text" />
+                {/* <form className="search-form">
+                    <input className="search-bar" type="text" value={search} onChange={updateSearch}/>
                     <button className="search-buttton" type="submit">Search</button>
-                </form>
+                </form> */}
                 <div className={classes.history}>
                     <h1>This week:</h1>
 
