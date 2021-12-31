ca90439075073221c740366e2006ff5aff144cb4
Åsa Berglund <you@example.com>
2021-12-31 11:39:41 +0100

    done a model and post req

--- signupbackend/models/SignupModel.js
+++ signupbackend/models/SignupModel.js
@@ -3,5 +3,23 @@ const mongoose = require('mongoose');
 //Here I create the model schema
 
 const SignupTemplate = new.mongoose.Schema({
-    
+    fullName:{
+        type: String,
+        required: true
+    },
+    userName:{
+        type:String,
+        required: true
+    },
+    password:{
+        type:String,
+        required: true
+    },
+    date:{
+        type: Date,
+        default: Date.now
+    }
 })
+
+//Exporting the model, taking two argumnet, the name of the table and the name of the schema that has been created. 
+module.exports = mongoose.model('myTable', SignupTemplate)
--- signupbackend/routes/routes.js
+++ signupbackend/routes/routes.js
@@ -2,11 +2,20 @@
 const { response } = require('express');
 const express = require('express');
 const router = express.Router()
+//importing the schema that has been created
+const SignupTemplateCopy = require('../models/SignupModel')
 
 //When a user press the submit button and send the form input, it sends a post request to this route. 
+//When the router post will run, all functions inside will also run. 
 //The first argument is the path, and the next argument is the callback function. 
 router.post('/signup', (req, res) =>{
-    response.send('send')
+    //Making an instance of the model, takeing all the post requests and putting the values into the schema. 
+    const SignupUser = new SignupTemplateCopy({
+        fullName: req.body.fullName,
+        userName: req.body.userName,
+        password: req.body.password,
+        
+    })
 })
 
 module.exports = router
