1f5ba0d78ae40a64d8f91abd941419e1e5130fbc
Per Berglund <pberglund@spotify.com>
2022-01-14 15:53:10 +0100

    Example of how to setup proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+    app.use(
+        ['/api*','/auth*'],
+        createProxyMiddleware({
+            target: 'https://mysessionlogapi.herokuapp.com',
+            // target: 'http://localhost:3001',
+            changeOrigin: true,
+        })
+    );
+};
